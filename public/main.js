e9605f810dc09e81a573fefb8361019da7722a64
Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
2019-03-26 00:09:23 +0300

    General Cleanup & load dependencies with revealjs and reformating

--- public/main.js
+++ public/main.js
@@ -2,6 +2,9 @@ Reveal.initialize({
     history: true,
     width: '100%',
     height: '100%',
+    math : {
+        mathjax : './mathjax/MathJax.js'
+    }
     /*dependencies: [
         //{ src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
         //{ src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
